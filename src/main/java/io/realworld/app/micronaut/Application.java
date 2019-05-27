bf60a7cbb87012f81fa2eeb54033014bff28debf
mirjahal <almirjr.87@gmail.com>
2019-05-27 20:55:19 -0300

    First commit

---
+++ src/main/java/io/realworld/app/micronaut/Application.java
@@ -0,0 +1,10 @@
+package io.realworld.app.micronaut;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
